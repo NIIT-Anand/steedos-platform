438b1359f480395d1b68246d3e4a44e78673fe23
Anand Pilania <Anand.Pilania@niit.com>
2024-05-14 04:29:43 +0000

    WIP

--- steedos-packages/webapp-accounts/webapp/src/components/Logo.tsx
+++ steedos-packages/webapp-accounts/webapp/src/components/Logo.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: Anand Pilania Anand.Pilania@niit.com
+ * @Date: 2024-05-14 04:09:47
+ * @LastEditors: Anand Pilania Anand.Pilania@niit.com
+ * @LastEditTime: 2024-05-14 04:29:32
+ * @FilePath: /steedos-platform/steedos-packages/webapp-accounts/webapp/src/components/Logo.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import * as React from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
@@ -15,7 +23,7 @@ const Logo = ({ tenant, location }: any) => {
     logoUrl = tenant.logo_url
   }
   return (<div>
-    <img src={logoUrl} className='h-10'></img>
+    <img src={logoUrl} className='logo-class h-10'></img>
     </div>
   )
 };
